d4fc0430057a94b5510ff9da4988e2df02d29a2b
colinmatthews <matthews.colin22@gmail.com>
2022-03-12 18:52:00 -0500

    move blocks into separate file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import PasswordReset from '../views/PasswordReset.vue'
+import Account from '../views/Account.vue'
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 Vue.use(VueRouter)
 
@@ -8,15 +13,43 @@ const routes = [
  
   {
     path: '/editor-double/:id',
-    component: () => import(/* webpackChunkName: "about" */ '../views/EditorDouble.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditorDouble.vue'),
+    meta:{
+      authRequired:true
+    }
   },
   {
     path: '/editor-single/:id',
-    component: () => import(/* webpackChunkName: "about" */ '../views/EditorSingle.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditorSingle.vue'),
+    meta:{
+      authRequired:true
+    }
   },
   {
     path: '/',
-    component: Home
+    component: Home,
+    name:"Home",
+    meta:{
+      authRequired:true
+    }
+   
+  },
+  {
+    path: '/login',
+    component: Login,
+    name:"Login"
+  },
+  {
+    path: '/reset',
+    component: PasswordReset
+  },
+  {
+    path: '/account',
+    component: Account,
+    name:"Account",
+    meta:{
+      authRequired:true
+    },
   }
 
 ]
@@ -24,5 +57,38 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+      // get firebase current user
+      const firebaseCurrentUser = user
+
+       if (
+           to.path === "/login" ||
+           to.path === "/error-404" ||
+           to.path === "/error-500" ||
+      //     to.path === "/callback" ||
+      //     to.path === "/pages/comingsoon" ||
+           (firebaseCurrentUser)
+       ) {
+           return next();
+       }
+
+      // If auth required, check login. If login fails redirect to login page
+      if(to.meta.authRequired) {
+        if (!(firebaseCurrentUser)) {
+          router.push({ path: '/login', query: { to: to.path } })
+        }
+      }
+
+      return next()
+      // Specify the current path as the customState parameter, meaning it
+      // will be returned to the application after auth
+      // auth.login({ target: to.path });
+
+  });
+
+});
 
 export default router
---
+++ src/store/blocks.js
@@ -0,0 +1,176 @@
+export default{
+   data:[
+        {
+          id:1,
+          title:"Canvas",
+          image:null,
+          component:"Canvas",
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:2,
+          title:"Paragraph",
+          image:null,
+          component:"Paragraph",
+          html:"",
+          openTag:"",
+          closeTag:"",
+          type:"plaintext"
+          
+        },
+        {
+          id:3,
+          title:"Allergies",
+          image:null,
+          component:"Allergies",
+          html:`<div class="ddemrcontent" dd:contenttype="ALLERGIES" dd:referenceuuid="1446A201-FD91-4A82-B645-306DC948DC48"></div>`,
+          openTag:"",
+          closeTag:""
+        },
+        {
+          id:4,
+          title:"ColumnLarge",
+          image:null,
+          component:"ColumnLarge",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:5,
+          title:"ColumnSmall",
+          image:null,
+          component:"ColumnSmall",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:6,
+          title:"Assessment and Plan",
+          image:null,
+          component:"AssessmentAndPlan",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:7,
+          title:"Chief Complaint",
+          image:null,
+          component:"ChiefComplaint",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:8,
+          title:"Hospital Course",
+          image:null,
+          component:"HospitalCourse",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:9,
+          title:"Lab Results",
+          image:null,
+          component:"LabResults",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:10,
+          title:"Medications",
+          image:null,
+          component:"Medications",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:11,
+          title:"Physical Exam",
+          image:null,
+          component:"PhysicalExam",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:12,
+          title:"Problems",
+          image:null,
+          component:"Problems",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:13,
+          title:"Review of Symptoms",
+          image:null,
+          component:"ReviewOfSymptoms",
+          html:``,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:14,
+          title:"Advanced Directive (Patient)",
+          image:null,
+          component:"AdvancedDirectivePatient",
+          html:`<div class="ddemrcontent" dd:contenttype="PATCARE_MEAS" dd:referenceuuid="BFD90E95-27DF-4AEE-989C-6A7AF37BC5FA"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:15,
+          title:"Advanced Directive (Physician)",
+          image:null,
+          component:"AdvancedDirectivePhys",
+          html:`<div class="ddemrcontent" dd:contenttype="PATCARE_MEAS" dd:referenceuuid="2001A4E5-8086-469C-8F15-7C3E62C9D70A"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:16,
+          title:"Assessment and Plan (Diagnosis Only)",
+          image:null,
+          component:"AssessmentAndPlanDiagnosis",
+          html:`<div class="ddemrcontent" dd:contenttype="DOC_COMP" dd:referenceuuid="DEB03B3D-7ED9-449D-B8E2-334F5AA2996D" dd:concept_cki="CERNER!3AAB66F1-295B-4ADA-BE1C-D2E29461E861"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:17,
+          title:"Assessment and Plan (Inpatient)",
+          image:null,
+          component:"AssessmentAndPlanInpatient",
+          html:`<div class="ddemrcontent" dd:contenttype="DOC_COMP" dd:referenceuuid="DEB03B3D-7ED9-449D-B8E2-334F5AA2996D" dd:concept_cki="CERNER!3AAB66F1-295B-4ADA-BE1C-D2E29461E861"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:18,
+          title:"Assessment and Plan (Outpatient)",
+          image:null,
+          component:"AssessmentAndPlanOutpatient",
+          html:`<div class="ddemrcontent" dd:contenttype="DOC_COMP" dd:referenceuuid="DEB03B3D-7ED9-449D-B8E2-334F5AA2996D" dd:concept_cki="CERNER!3AAB66F1-295B-4ADA-BE1C-D2E29461E861"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        },
+        {
+          id:19,
+          title:"Behavioural Health Issues",
+          image:null,
+          component:"BehaviouralHealthIssues",
+          html:`<div class="ddemrcontent" dd:contenttype="DOC_COMP" dd:referenceuuid="DEB03B3D-7ED9-449D-B8E2-334F5AA2996D" dd:concept_cki="CERNER!3AAB66F1-295B-4ADA-BE1C-D2E29461E861"></div>`,
+          openTag:"<div>",
+          closeTag:"</div>"
+        }
+      ]
+}
