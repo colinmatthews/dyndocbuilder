85e8685083a6a695e1edc3f9e0be2752e448eba6
colinmatthews <matthews.colin22@gmail.com>
2022-03-12 18:53:01 -0500

    add firebase, axios, env config

---
+++ src/helpers/firebase.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyCC-XEBDJEJtpOJUp1xEsfltQasaJoS3rc",
+    authDomain: "physdoc-58729.firebaseapp.com",
+    projectId: "physdoc-58729",
+    storageBucket: "physdoc-58729.appspot.com",
+    messagingSenderId: "151125826368",
+    appId: "1:151125826368:web:33990aad8a06ebdb8bf8a5",
+    measurementId: "G-CGPS3WZXQK"  
+}
--- src/main.js
+++ src/main.js
@@ -5,17 +5,29 @@ import store from './store'
 import './helpers/global-components'
 
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 import './assets/tailwind.css'
+import firebaseConfig from "./helpers/firebase"
+import axios from 'axios'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
-Vue.config.productionTip = false
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
+
+let HTTPClient = axios.create({ baseURL: 'http://localhost:8081/'});
+Vue.prototype.$http = HTTPClient;
+store.$http = HTTPClient
+
+Vue.config.productionTip = false
 
 new Vue({
   store,
